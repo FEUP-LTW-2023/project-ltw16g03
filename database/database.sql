19fd3d14ba27ff230bb0c0d70b649379cb157961
rafazalves <rafael.alves238@gmail.com>
2023-04-23 20:54:53 +0100

    database corrections

--- database/database.sql
+++ database/database.sql
@@ -9,10 +9,6 @@ DROP TABLE IF EXISTS Hashtag;
 DROP TABLE IF EXISTS Ticket_Hashtag;
 DROP TABLE IF EXISTS Faq;
 
---TYPES
-
-CREATE TYPE Status AS ENUM ('Open','Assigned','Resolved', 'Closed');
-
 --Tables
 
 CREATE TABLE users (
@@ -35,9 +31,9 @@ CREATE TABLE Client (
 
 CREATE TABLE Agent (
     id INTEGER REFERENCES users (id) ON DELETE CASCADE,
-    id_department INTEGER REFERENCES Department (id),
+    id_department INTEGER,
     CONSTRAINT id_agentPK PRIMARY KEY(id),
-    CONSTRAINT id_departmentFK FOREIGN KEY(id_department)
+    CONSTRAINT id_departmentFK FOREIGN KEY(id_department) REFERENCES Department (id)
 );
 
 CREATE TABLE admins (
@@ -50,45 +46,45 @@ CREATE TABLE Task (
     title varchar(255) NOT NULL,
     description TEXT NOT NULL,
     is_completed BOOLEAN NOT NULL DEFAULT False,
-    id_agent INTEGER REFERENCES Agent (id),
+    id_agent INTEGER,
     CONSTRAINT id_taskPK PRIMARY KEY(id),
-    CONSTRAINT id_agentFK FOREIGN KEY(id_agent) ON DELETE CASCADE
+    CONSTRAINT id_agentFK FOREIGN KEY(id_agent) REFERENCES Agent (id) ON DELETE CASCADE
 );
 
 CREATE TABLE Department (
     id SERIAL,
     name varchar(255) NOT NULL UNIQUE,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Ticket (
     id SERIAL,
     title varchar(255) NOT NULL UNIQUE,
     description TEXT NOT NULL,
-    ticket_status Status DEFAULT 'Open',
+    ticket_status VARCHAR (10)  NOT NULL DEFAULT 'Open' CHECK (ticket_status IN ('Open', 'Assigned', 'Resolved', 'Closed')),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    id_client INTEGER REFERENCES Client (id),
-    id_agent INTEGER REFERENCES Agent (id),
-    id_department INTEGER REFERENCES Department (id),
+    id_client INTEGER,
+    id_agent INTEGER,
+    id_department INTEGER,
     CONSTRAINT id_ticketPK PRIMARY KEY(id),
     CONSTRAINT current_edit_date_ck CHECK (created_at <= updated_at),
-    CONSTRAINT id_clientFK FOREIGN KEY(id_client),
-    CONSTRAINT id_agentFK FOREIGN KEY(id_agent),
-    CONSTRAINT id_departmentFK FOREIGN KEY(id_department)
+    CONSTRAINT id_clientFK FOREIGN KEY(id_client) REFERENCES Client (id),
+    CONSTRAINT id_agentFK FOREIGN KEY(id_agent) REFERENCES Agent (id),
+    CONSTRAINT id_departmentFK FOREIGN KEY(id_department) REFERENCES Department (id)
 );
 
 CREATE TABLE Hashtag (
     id SERIAL,
-    tag varchar(255) NOT NULL UNIQUE,
+    tag varchar(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE Ticket_Hashtag (
-   id_ticket INTEGER REFERENCES Ticket (id),
-   id_hashtag INTEGER REFERENCES Hashtag (id),
+   id_ticket INTEGER,
+   id_hashtag INTEGER,
    CONSTRAINT ticket_hashtagPK PRIMARY KEY(id_ticket, id_hashtag),
-   CONSTRAINT id_ticketFK FOREIGN KEY(id_ticket),
-   CONSTRAINT id_hashtagFK FOREIGN KEY(id_hashtag)
+   CONSTRAINT id_ticketFK FOREIGN KEY(id_ticket) REFERENCES Ticket (id),
+   CONSTRAINT id_hashtagFK FOREIGN KEY(id_hashtag) REFERENCES Hashtag (id)
 );
 
 CREATE TABLE Faq (
