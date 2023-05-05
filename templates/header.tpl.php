577e76b08be582d4481c51a71082bc6fb32e9a85
Rafael Alves <rafael.alves238@gmail.com>
2023-05-05 21:14:53 +0100

    search departments done

--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -14,6 +14,7 @@
         <link rel="stylesheet" href="../css/novo_ticket.css"> 
         <link rel="stylesheet" href="../css/login.css"> 
         <link rel="stylesheet" href="../css/register.css">
+        <script src="../javascript/script.js" defer></script>
     </head>
     <body>
         <header>
